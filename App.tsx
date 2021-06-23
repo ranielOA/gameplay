dca1e96e91d4f75e075c0afe78ff0116c465ec53
ranielOA <r.a_tw@hotmail.com>
2021-06-22 21:51:00 -0300

    added routes

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ import {
 import AppLoading from "expo-app-loading";
 import { useFonts } from "expo-font";
 
-import { Home } from "./src/screens/Home";
+import { Routes } from "./src/routes";
 import { Background } from "./src/components/Background";
 
 export default function App() {
@@ -30,7 +30,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      <Home />
+      <Routes />
     </Background>
   );
 }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { Text, View, Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 import { ButtonIcon } from "../../components/ButtonIcon";
 import IllustrationImg from "../../assets/illustration.png";
 import { styles } from "./styles";
 
 export function SignIn() {
+  const navigation = useNavigation();
+
+  function handleSignIn() {
+    navigation.navigate("Home");
+  }
+
   return (
     <View style={styles.container}>
       <Image
@@ -25,7 +32,7 @@ export function SignIn() {
           favoritos com seus amigos
         </Text>
 
-        <ButtonIcon title="Entrar com o Discord" activeOpacity={0.7} />
+        <ButtonIcon title="Entrar com o Discord" onPress={handleSignIn} />
       </View>
     </View>
   );
